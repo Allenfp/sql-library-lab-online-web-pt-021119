6e096d27358088f8b937e77aba8589fc6c6a4f9d
Allen F Partlow <allenfp@gmail.com>
2019-04-22 04:52:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) a FROM characters GROUP BY a ORDER BY a DESC LIMIT 1; "
+  "SELECT characters.species, COUNT(*) a FROM characters GROUP BY a ORDER BY a DESC LIMIT 1; "
 end
 
 def select_name_and_series_subgenres_of_authors
