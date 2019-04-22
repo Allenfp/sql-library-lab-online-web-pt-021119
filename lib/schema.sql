7d5eb39fa5583d04f72e1a4cdd234141f333a6a0
Allen F Partlow <allenfp@gmail.com>
2019-04-22 04:04:29 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,11 +14,11 @@ CREATE TABLE subgenres (
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT
-)
+);
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   year INTEGER,
   series_id INTEGER,
   title TEXT
-)
+);
